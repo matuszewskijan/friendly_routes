874894a03f357ada3a1be9ea46890e55d234f4f8
Maxim Perepelitsa <n0xff@outlook.com>
2022-01-14 02:59:31 +0300

    Pretty tests

--- spec/unit/services/prefixed_params_spec.rb
+++ spec/unit/services/prefixed_params_spec.rb
@@ -4,28 +4,34 @@ describe FriendlyRoutes::PrefixedParams do
     @param_name = Faker::Hipster.word
     @param_options = { true: Faker::Hipster.word, false: Faker::Hipster.word }
   end
+
   def boolean(name, options, optional: false)
     FriendlyRoutes::Params::BooleanParams.new(name, options, optional: optional)
   end
 
   describe '#to_s' do
     subject { FriendlyRoutes::PrefixedParams.new(@params, @prefix).to_s }
+
     context 'required param' do
       before do
         @params = [boolean(@param_name, @param_options, optional: false)]
       end
+
       it 'should add required condition to path' do
         is_expected.to eq(":#{@prefix}_#{@param_name}")
       end
     end
+
     context 'optional param' do
       before do
         @params = [boolean(@param_name, @param_options, optional: true)]
       end
+
       it 'should add optional condition to path' do
         is_expected.to eq("(:#{@prefix}_#{@param_name})")
       end
     end
+
     context 'string' do
       before do
         @param = Faker::Lorem.word
